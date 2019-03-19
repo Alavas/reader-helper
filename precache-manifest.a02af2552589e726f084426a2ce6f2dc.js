3366fffc2d32eecc501074cdcc600886f93192d6
alavas <savala.justin@gmail.com>
2019-03-18 21:59:40 -0400

    Updates

---
+++ precache-manifest.a02af2552589e726f084426a2ce6f2dc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0f3b3b4bc57b9a3f5797",
+    "url": "/usb_card_reader/static/js/runtime~main.7f30fada.js"
+  },
+  {
+    "revision": "148146f892427ff80c0f",
+    "url": "/usb_card_reader/static/js/main.2f9ad27c.chunk.js"
+  },
+  {
+    "revision": "6f8012fc29a3b3dcc13e",
+    "url": "/usb_card_reader/static/js/2.a71dd8ef.chunk.js"
+  },
+  {
+    "revision": "148146f892427ff80c0f",
+    "url": "/usb_card_reader/static/css/main.b19222fa.chunk.css"
+  },
+  {
+    "revision": "c2fe045ca82d0f4a806005e6a9ef3a5f",
+    "url": "/usb_card_reader/index.html"
+  }
+];
